0fae96e3c2f1b089a2c71ed283c286515cf809c3
Sangwon Song <ssong86@gatech.edu>
2017-09-30 04:11:41 -0400

    final commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,44 +26,50 @@ var minutesTillTrain = "";
 var keyHolder = "";
 var getKey = "";
 
-$("#add-train").on("click", function(){
-   event.preventDefault();
+$(document).ready(function(){
 
-   trainName = $("#name-input").val().trim();
-   trainDestination = $("#destination-input").val().trim();
-   firstTrainTime = $("#first-train-time-input").val().trim();
-   trainFrequency = $("#frequency-input").val().trim();
-   currentTime = moment();
-   diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-   tRemainder = diffTime % trainFrequency;
-   minutesTillTrain = trainFrequency - tRemainder;
-   nextTrain = moment().add(minutesTillTrain, "minutes");
-   nextTrainFormatted = moment(nextTrain).format("hh:mm");
+    $("#add-train").on("click", function(){
+       event.preventDefault();
 
-   //database push
-    database.ref().push({
-        data_name: trainName,
-        data_destination: trainDestination,
-        data_frequency: trainFrequency,
-        data_firstTrainTime: firstTrainTime,
-        data_nextTrainFormatted: nextTrainFormatted,
-        data_minutesTillTrain: minutesTillTrain
+       trainName = $("#name-input").val().trim();
+       trainDestination = $("#destination-input").val().trim();
+       firstTrainTime = $("#first-train-time-input").val().trim();
+       trainFrequency = $("#frequency-input").val().trim();
+       firstTimeConverted=moment(firstTrainTime, "hh:mm").subtract(1, "years");
+       currentTime = moment();
+       diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+       tRemainder = diffTime % trainFrequency;
+       minutesTillTrain = trainFrequency - tRemainder;
+       nextTrain = moment().add(minutesTillTrain, "minutes");
+       nextTrainFormatted = moment(nextTrain).format("hh:mm");
+
+       //database push
+        database.ref().push({
+            data_name: trainName,
+            data_destination: trainDestination,
+            data_frequency: trainFrequency,
+            data_firstTrainTime: firstTrainTime,
+            data_nextTrainFormatted: nextTrainFormatted,
+            data_minutesTillTrain: minutesTillTrain
+        });
+        //refresh the inputs
+        //$("#name-input").val('');
+        //$('#destination-input').val('');
+        //$("#first-train-time-input").val('');
+        //$("#frequency-input").val('');
+
+        return false;
     });
-    //refresh the inputs
-    //$("#name-input").val('');
-    //$('#destination-input').val('');
-    //$("#first-train-time-input").val('');
-    //$("#frequency-input").val('');
 
-    return false;
-});
+    database.ref().on("child_added", function(snapshot){
+        $("#train-schedule").append("<tr><td id='name-display'>"+snapshot.val().data_name+
+            "</td><td id='destination-display'>"+snapshot.val().data_destination+
+            "</td><td id='frequency-display'>"+snapshot.val().data_frequency+
+            "</td><td id='nextArrival-display'>"+snapshot.val().data_nextTrainFormatted+
+            "</td><td id='minutes-display'>"+snapshot.val().data_minutesTillTrain+"</td></tr>");
+    }, function(errorObject){
+        console.log("Errors Handled: "+ errorObject.code);
+    });
 
-database.ref().on("child_added", function(snapshot){
-    $("#train-schedule").append("<tr><td id='name-display'>"+snapshot.val().data_name+
-        "</td><td id='destination-display'>"+snapshot.val().data_destination+
-        "</td><td id='frequency-display'>"+snapshot.val().data_frequency+
-        "</td><td id='nextArrival-display'>"+snapshot.val().data_nextTrainFormatted+
-        "</td><td id='minutes-display'>"+snapshot.val().data_minutesTillTrain+"</td></tr>");
-}, function(errorObject){
-    console.log("Errors Handled: "+ errorObject.code);
+
 });
